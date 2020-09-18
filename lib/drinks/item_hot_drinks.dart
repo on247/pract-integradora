7fd3b7cdaf4fe888f03297a514a485b3dff8d0a9
Alonso L <me@on247.me>
2020-09-18 18:20:05 -0500

    Lista de bebidas

--- lib/drinks/item_hot_drinks.dart
+++ lib/drinks/item_hot_drinks.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:estructura_practica_1/models/product_hot_drinks.dart';
+import 'package:estructura_practica_1/utils/constants.dart';
 
 class ItemHotDrinks extends StatefulWidget {
   final ProductHotDrinks drink;
@@ -15,6 +16,92 @@ class ItemHotDrinks extends StatefulWidget {
 class _ItemHotDrinksState extends State<ItemHotDrinks> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("${widget.drink.productTitle}"));
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 16, 30, 16),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height / 4,
+          child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: ITEM_BACKGROUND_DARK,
+                  spreadRadius: -3,
+                  offset: Offset(1, 1),
+                  blurRadius: 2,
+                ),
+              ],
+            ),
+            child: Card(
+              child: Stack(
+                children: [
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width / 3,
+                        child: Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Café",
+                                  style: Theme.of(context).textTheme.headline6,
+                                ),
+                                Text(
+                                  "${widget.drink.productTitle}",
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                                Expanded(child: Container()),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 0, 0, 16),
+                                  child: Text(
+                                    "\$ ${widget.drink.productPrice.toInt()}",
+                                    style:
+                                        Theme.of(context).textTheme.headline4,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(8, 8, 48, 8),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: SizedBox(
+                                  child: Image.network(
+                                    widget.drink.productImage,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.thumb_up,
+                      ),
+                      onPressed: null,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
