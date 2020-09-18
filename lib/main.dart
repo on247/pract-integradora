93a6cd70af49f9e55dd1cfcaaa68ab04e759fb8a
Alonso L <me@on247.me>
2020-09-18 18:53:34 -0500

    buttonTheme

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,20 @@ class MyApp extends StatelessWidget {
       title: APP_TITLE,
       theme: ThemeData(
         primaryColor: PRIMARY_COLOR,
+        buttonTheme: Theme.of(context).buttonTheme.copyWith(
+              buttonColor: ITEM_BACKGROUND_DARK,
+              hoverColor: ITEM_BACKGROUND,
+              highlightColor: ITEM_BACKGROUND,
+              textTheme: ButtonTextTheme.accent,
+              colorScheme: Theme.of(context).colorScheme.copyWith(
+                    secondary: Colors.white,
+                  ), // T
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+            ),
         cardColor: ITEM_BACKGROUND,
         textTheme: Theme.of(context).textTheme.copyWith(
               headline4: Theme.of(context).textTheme.headline4.copyWith(
