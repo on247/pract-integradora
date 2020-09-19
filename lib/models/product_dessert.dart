2623c82ed66959aed4147fed0cb386fb64cbe544
Alonso L <me@on247.me>
2020-09-18 19:41:07 -0500

    Modelos y datos Postres

--- lib/models/product_dessert.dart
+++ lib/models/product_dessert.dart
@@ -1,2 +1,53 @@
 // TODO: Crear producto "ProductCup" siguiendo la logica de Drinks y Grains
 // TODO: Agregar al ProductRepository una lista de estos productos.
+import 'dart:math';
+import 'package:flutter/foundation.dart';
+
+enum DessertSize { CH, M, G }
+
+class ProductDessert {
+  final String productTitle; // nombre del producto
+  final String productDescription; // descripcion del producto
+  final String productImage; // url de imagen del producto
+  DessertSize productSize; // tamano del producto
+  double productPrice; // precio del producto autocalculado
+  final int productAmount; // cantidad de producto por comprar
+  final bool liked;
+
+  ProductDessert({
+    @required this.productTitle,
+    @required this.productDescription,
+    @required this.productImage,
+    @required this.productSize,
+    @required this.productAmount,
+    this.liked = false,
+  }) {
+    // inicializa el precio de acuerdo a la size del producto
+    productPrice = productPriceCalculator();
+  }
+
+  // Mandar llamar este metodo cuando se cambie el tamanio del producto
+  // De esta manera el precio del nuevo tamanio del producto se autocalcula
+  // Por ejemplo cuando se cambie a M
+  //
+  // FlatButton(
+  //   child: Text("M"),
+  //   onPressed: () {
+  //     setState(() {
+  //       prod.productSize = ProductSize.M;
+  //       prod.productPrice = prods.productPriceCalculator();
+  //     });
+  //   },
+  // ),
+  //
+  //
+  double productPriceCalculator() {
+    if (this.productSize == DessertSize.CH)
+      return (50 + Random().nextInt(70)).toDouble();
+    if (this.productSize == DessertSize.M)
+      return (70 + Random().nextInt(90)).toDouble();
+    if (this.productSize == DessertSize.G)
+      return (90 + Random().nextInt(100)).toDouble();
+    return 999.0;
+  }
+}
